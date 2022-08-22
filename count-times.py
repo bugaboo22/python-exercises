e4fb5957a3f024195bcf4bdc07f2a2673ad734f1
Lauri Vastela <lauri.vastela@gmail.com>
2022-08-22 13:19:00 +0300

    update

---
+++ count-times.py
@@ -0,0 +1,21 @@
+
+
+text = open("mbox-short.txt", "r")
+
+d=dict()
+for line in text:
+    if not line.startswith("From "): 
+        continue
+    else :
+        line=line.split()
+        line=line[5]
+        line=line[0:2]
+        d[line]=d.get(line,0)+1
+
+lst=list()        
+for value,count in d.items():
+    lst.append((value,count))
+
+lst.sort()
+for value,count in lst:
+    print(value,count)
